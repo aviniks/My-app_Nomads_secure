a95bdc3399496e23120d618c32729be7dc2b9d18
abhishek03kr <abhishekkr.3232@outlook.com>
2026-07-10 23:12:34 +0530

    recent change

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -3,5 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_Supabase_URL_Nomads;
 const supabaseKey = import.meta.env.VITE_Supabase_ANON_key_nomads;
 
-console.log("URL = https://uscdurmsftxodmlwwxjw.supabase.co/rest/v1/", supabaseUrl);
+console.log("URL = https://uscdurmsftxodmlwwxjw.supabase.co", supabaseUrl);
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
